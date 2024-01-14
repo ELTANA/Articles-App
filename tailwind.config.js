5c7c990606cee9c53d3973a3307eb4f515827779
Lotanna Kyrian A <kyrian.lotanna@gmail.com>
2024-01-14 14:08:58 +0100

    chore : extend box shadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'article-card': 'rgba(100, 100, 111, 0.2) 0px 7px 29px 0px'
+      }
+    },
     fontFamily: {
       sans: ['Raleway']
     }
