66754368f0ca3821ed17dd49dc40c98696b37318
Lotanna Kyrian A <kyrian.lotanna@gmail.com>
2024-01-14 14:09:41 +0100

    chore : update code

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,17 +4,17 @@ import { Link } from '@tanstack/react-router';
 const Header = () => {
   return (
     <header className="py-10 flex justify-between items-center">
-      <Link className="inline-flex gap-2 font-medium items-center text-gray-900" href="/">
-        <ArticlesLogo className="w-10 h-10 text-white p-2 bg-blue-500 rounded-full" />
-        <span className="text-lg">Articles</span>
+      <Link className="inline-flex gap-2 font-medium items-center text-gray-900" to="/">
+        <ArticlesLogo className="w-7 md:w-10 h-7 md:h-10 text-white p-2 bg-blue-500 rounded-full" />
+        <span className="text-base md:text-lg">Articles</span>
       </Link>
       <nav className="gap-4 flex flex-wrap items-center text-base justify-center">
-        <Link className="text-base truncate text-gray-600 font-medium" href="/">
+        <Link className="text-base truncate text-gray-600 font-medium" to="/">
           Results
         </Link>
         <Link
           className="inline-flex gap-1 items-center bg-blue-100 border-0 py-2 px-4 focus:outline-none hover:bg-blue-200 rounded text-base text-gray-800 font-medium"
-          href="/create"
+          to="/create"
         >
           Create
         </Link>
