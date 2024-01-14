c201745fe03488876475a741cafe68c2562bbfde
Lotanna Kyrian A <kyrian.lotanna@gmail.com>
2024-01-14 09:01:16 +0100

    feat : build ArticleCreation screen

---
+++ src/screens/ArcticleCreation/ArcticleCreation.tsx
@@ -0,0 +1,48 @@
+import { FC } from 'react';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useForm } from 'react-hook-form';
+import Input from '$components/Input/Input';
+import TextArea from '$components/Input/TextArea';
+import Button from '$components/Button/Button';
+import { ArticleCreationFormType, articleCreationFormSchema } from './ArticleCreation.types';
+
+const ArcticleCreation: FC = () => {
+  //form validation
+  const {
+    register,
+    formState: { errors },
+    handleSubmit
+  } = useForm<ArticleCreationFormType>({
+    mode: 'all',
+    resolver: yupResolver(articleCreationFormSchema),
+    defaultValues: {
+      author: '',
+      email: '',
+      phoneNumber: '',
+      snippet: ''
+    }
+  });
+
+  return (
+    <section className="grow py-5 max-w-[768px] w-full mx-auto">
+      <h1 className="text-gray-900 text-xl sm:text-4xl mb-2 font-semibold">Create an article</h1>
+      <p className="leading-relaxed mb-10 text-gray-600">
+        Fill the form with details of your article, so it can be added to the articles list.
+      </p>
+      <form onSubmit={handleSubmit(() => {})} className="flex flex-col items-start gap-4">
+        <Input {...register('author')} error={errors.author} name="author" label="Author" />
+        <Input {...register('email')} error={errors.email} name="email" label="Email" type="email" />
+        <Input {...register('phoneNumber')} error={errors.phoneNumber} name="phoneNumber" label="Phone number" />
+        <TextArea
+          {...register('snippet')}
+          error={errors.snippet}
+          name="snippet"
+          label="Snippet (Max — 300 characters)"
+        />
+        <Button type="submit" text="Create Article" />
+      </form>
+    </section>
+  );
+};
+
+export default ArcticleCreation;
---
+++ src/screens/ArcticleCreation/ArticleCreation.types.ts
@@ -0,0 +1,13 @@
+import * as yup from 'yup';
+
+export const articleCreationFormSchema = yup.object({
+  email: yup.string().trim().email('The email is not valid').required('This field is required'),
+  phoneNumber: yup.string().trim().required('This field is required'),
+  author: yup.string().trim().required('This field is required'),
+  snippet: yup
+    .string()
+    .trim()
+    .max(300, 'The snippet should not be more than 300 characters')
+    .required('This field is required')
+});
+export type ArticleCreationFormType = yup.InferType<typeof articleCreationFormSchema>;
