5493bb2950e7013103c8dde4dde2bd4378d3d2e3
Lotanna Kyrian A <kyrian.lotanna@gmail.com>
2024-01-14 11:40:37 +0100

    chore : update endpoint

--- src/network/axios.ts
+++ src/network/axios.ts
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://my-json-server.typicode.com/user/repo' as const;
+const BASE_URL = 'https:///my-json-server.netlify.app' as const;
+// const BASE_URL = 'http://localhost:3000' as const;
+// const BASE_URL = 'https://my-json-server.typicode.com/ELTANA/Articles-App' as const;
 
 const instance = axios.create({
   baseURL: BASE_URL
