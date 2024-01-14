bbbe42c4247f089ef8507e6f69a204abcac3598d
Lotanna Kyrian A <kyrian.lotanna@gmail.com>
2024-01-14 08:59:55 +0100

    feat : create Footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="py-4 mt-3 border-t-2 border-t-blue-700">
+      <p className="text-center">© 2024 Articles App — Lotana</p>
+    </footer>
+  );
+};
+
+export default Footer;
