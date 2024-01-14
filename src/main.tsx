f6d38ca8eb5ea1a5d21333ea026bfc75f0f4d88e
Lotanna Kyrian A <kyrian.lotanna@gmail.com>
2024-01-14 10:09:48 +0100

    chore : setup react query

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { RouterProvider } from '@tanstack/react-router';
 import router from './App.tsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+//Create a client
+const queryClient = new QueryClient();
 
 const rootElement = document.getElementById('root')!;
 if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
     <StrictMode>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </StrictMode>
   );
 }
