cbba4393130d05d32dd915ab07aadba4430be728
Lotanna Kyrian A <kyrian.lotanna@gmail.com>
2024-01-14 11:41:09 +0100

    feat : implement get articles

--- src/network/article.ts
+++ src/network/article.ts
@@ -1,8 +1,14 @@
 import type { Article } from 'src/utils/global.types';
 import instance from './axios';
 import Endpoints from './endpoints';
+import type { AxiosResponse } from 'axios';
 
-export const postArticle = async (payload: Article): Promise<string> => {
-  const res = await instance.post(Endpoints.ARTICLES, payload);
+export const postArticle = async (payload: Omit<Article, 'id'>): Promise<Article> => {
+  const res = await instance.post<Article, AxiosResponse<Article>>(Endpoints.ARTICLES, payload);
+  return res.data;
+};
+
+export const getArticles = async (): Promise<Article[]> => {
+  const res = await instance.get<Article[]>(Endpoints.ARTICLES);
   return res.data;
 };
