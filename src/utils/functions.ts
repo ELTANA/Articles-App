66dd011b3248753085ccde92cdd53e346c7cf07b
Lotanna Kyrian A <kyrian.lotanna@gmail.com>
2024-01-15 08:09:17 +0100

    chore : add util function generate unique strings

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -4,3 +4,10 @@ export const truncateText = (text: string, limit: number) => {
     isTruncated: text && text.length > limit
   };
 };
+
+export const generateUniqueString = () => {
+  const timestampPart = Date.now().toString(36);
+  const randomPart = Math.random().toString(36).substring(2, 9); // 7 characters
+
+  return `${timestampPart}${randomPart}`.toUpperCase().substring(0, 7);
+};
